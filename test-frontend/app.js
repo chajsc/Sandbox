b854d28cced566d6da3cb40d729ae55fd4a0e3fe
Johnney <chajsc@gmail.com>
2018-08-06 11:30:00 -0500

    commit changes

---
+++ test-frontend/app.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env nodejs
+var http = require('http');
+var ip = "127.0.0.1";
+http.createServer(function (request, response) {
+  // Send the HTTP header 
+  // HTTP Status: 200 : OK
+  // Content Type: text/plain
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  
+  // Send the response body as "Hello World"
+  response.end('Hello World\n');
+}).listen(8080, ip);
+
+// Console will print the message
+console.log('Server running at '+ip+':8080/');
